0bdfccf2304c931825765940096ecf1b3516c4b3
Zack Scholl <zack.scholl@gmail.com>
2022-01-20 11:01:41 -0800

    whoops

---
+++ lib/patterns.lua
@@ -0,0 +1,65 @@
+local drum_patterns={}
+drum_patterns["a blank"]=[[]]
+drum_patterns["weird fishes"]=[[
+  bd x-----xx--
+  rc --x--x--x-
+  cs --x-x-x-x-
+  cs --x--x--x-
+  ch x-x-x-xxx-
+]]
+
+drum_patterns["amen brother"]=[[
+  bd x-x-------xx----
+  sd ----x--x-x--x--x
+  rc x-x-x-x-x-x-x-x-
+  oh ----------------
+  hh ----------------
+  t1 ----------------
+  t2 ----------------
+  t3 ----------------
+ 
+  bd x-x-------xx----
+  sd ----x--x-x--x--x
+  rc x-x-x-x-x-x-x-x-
+  oh ----------------
+  hh ----------------
+  t1 ----------------
+  t2 ----------------
+  t3 ----------------
+ 
+  bd x-x-------x-----
+  sd ----x--x-x-----x
+  rc x-x-x-x-x-x-x-x-
+  oh ----------------
+  hh ----------------
+  t1 ----------------
+  t2 ----------------
+  t3 ----------------
+ 
+  bd --xx------x-----
+  sd -x--x--x-x----x-
+  rc x-x-x-x-x---x-x-
+  oh ----------x-----
+  hh -----------x----
+  t1 ---------xx-----
+  t2 -----------xx---
+  t3 -------------xxx
+]]
+
+-- https://repositori.upf.edu/bitstream/handle/10230/44875/gomez_CMMR2017_drum.pdf?isAllowed=y&sequence=1
+
+drum_patterns["deep house"]=[[
+  oh --x---x---x---x-
+  ch ----x-xx----x-xx
+  bd x---x---x---x---
+  sd ---x-------x----
+]]
+
+drum_patterns["funk break"]=[[
+  oh ------x---x-----
+  ch xxxxxx--xx--xxxx
+  bd x--x--x-x---x---
+  sd ----x----x--x---
+]]
+
+return drum_patterns
